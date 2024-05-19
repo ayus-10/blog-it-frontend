1fe7c62b134b2f5d76e2380c0ae6407b7d04759d
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-05-19 20:36:10 +0545

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
+@Component({
+  selector: "app-root",
+  standalone: true,
+  imports: [RouterOutlet],
+  template: `<h1 class="text-3xl font-bold underline">Hello world!</h1>`,
+})
+export class AppComponent {}
