d1ea803ad90bc331207cef26e7ef50b05ff051b8
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2024-05-22 18:41:58 +0545

    Improved auth guard logic

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,14 +2,27 @@ import { inject } from "@angular/core";
 import { CanActivateFn, Router } from "@angular/router";
 import { AuthService } from "../auth/auth.service";
 
-export const AuthGuard: CanActivateFn = (route, state) => {
+export const AuthGuard: CanActivateFn = (route) => {
+  const nonProtectedRoutes = ["login", "signup"];
+
+  const currentPath = route.routeConfig?.path;
+
   const router = inject(Router);
   const authService = inject(AuthService);
 
-  if (authService.currentAuthToken() === null) {
-    return true;
+  const isAuthTokenNull = authService.currentAuthToken() === null;
+
+  if (currentPath && nonProtectedRoutes.includes(currentPath)) {
+    if (isAuthTokenNull) {
+      return true;
+    }
+    router.navigateByUrl("/");
+    return false;
   }
 
-  router.navigateByUrl("/");
-  return false;
+  if (isAuthTokenNull) {
+    router.navigateByUrl("/login");
+    return false;
+  }
+  return true;
 };
