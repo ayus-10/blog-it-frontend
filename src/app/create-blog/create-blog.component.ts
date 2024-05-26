71a02b2da2133c4622362e61c65cc2a1781cfec4
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2024-05-26 20:55:37 +0545

    Implemented form validation

--- src/app/create-blog/create-blog.component.ts
+++ src/app/create-blog/create-blog.component.ts
@@ -1,5 +1,14 @@
-import { Component } from "@angular/core";
-import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { Component, OnInit, inject } from "@angular/core";
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from "@angular/forms";
+import { ErrorMessageService } from "../error-message/error-message.service";
 
 @Component({
   selector: "app-create-blog",
@@ -7,7 +16,7 @@ import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
   imports: [ReactiveFormsModule],
   templateUrl: "./create-blog.component.html",
 })
-export class CreateBlogComponent {
+export class CreateBlogComponent implements OnInit {
   blogCategories = [
     "Lifestyle",
     "Technology",
@@ -24,14 +33,68 @@ export class CreateBlogComponent {
     "Other",
   ];
 
-  blogFormValues = new FormGroup({
-    title: new FormControl(""),
-    category: new FormControl(""),
-    image: new FormControl(""),
-    content: new FormControl(""),
-  });
+  errorMessageService = inject(ErrorMessageService);
 
-  handleSubmit() {
-    console.log(this.blogFormValues.value);
+  blogForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.blogForm = new FormGroup({
+      title: new FormControl("", [
+        Validators.required,
+        Validators.minLength(20),
+      ]),
+      category: new FormControl("", [
+        Validators.required,
+        this.validateCategory(),
+      ]),
+      image: new FormControl(""),
+      content: new FormControl("", [
+        Validators.required,
+        Validators.minLength(100),
+      ]),
+    });
+  }
+
+  validateCategory(): ValidatorFn {
+    return (contol: AbstractControl): ValidationErrors | null => {
+      const isValid = this.blogCategories.includes(contol.value);
+      if (isValid) {
+        return null;
+      }
+      return {
+        validSelection: false,
+      };
+    };
+  }
+
+  validationErrors() {
+    if (this.blogForm.valid) {
+      return false;
+    }
+    const { title, category, image, content } = this.blogForm.controls;
+    if (title.errors) {
+      this.setErrorMessage("Title must be at least 20 characters long");
+    } else if (category.errors) {
+      this.setErrorMessage("Selected category is invalid");
+    } else if (image.errors) {
+      this.setErrorMessage("");
+    } else if (content.errors) {
+      this.setErrorMessage("Content must contain at least 100 characters");
+    }
+    return true;
+  }
+
+  setErrorMessage(errorText: string) {
+    this.errorMessageService.alertText.set(errorText);
+    setTimeout(() => {
+      this.errorMessageService.alertText.set("");
+    }, 3000);
+  }
+
+  onSubmit() {
+    if (this.validationErrors()) {
+      return;
+    }
+    console.log(this.blogForm.value);
   }
 }
